bab97c187ce79e51088f48e49b574736c47b65e1
Nikita Kovalev <nick.tommybee37@gmail.com>
2024-12-14 18:24:27 +0100

    perform filename search

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use crate::command::extract_args;
 
 mod command;
 mod validation;
@@ -8,7 +7,19 @@ mod crawler;
 fn main() {
     let filefinder_cmd = command::filefinder_cmd();
 
-    let args_map: HashMap<String, String> = extract_args(&filefinder_cmd);
+    let args_map: HashMap<String, String> = command::extract_args(&filefinder_cmd);
 
     println!("Args: {:?}", args_map);
+
+    if args_map.contains_key("name") {
+        let directory = args_map.get("directory").unwrap();
+        let query = args_map.get("query").unwrap();
+
+        let result = crawler::search_by_filename(directory, query);
+
+        match result {
+            Some(file) => println!("File found: {}", file),
+            None => println!("File not found")
+        }
+    }
 }
